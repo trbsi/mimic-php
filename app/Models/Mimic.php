74303269692b753f964fdafd4693515da903a3ea
Dario Trbovic <timmy.dario@gmail.com>
2017-06-20 23:30:58 +0200

    working on listing mimics

--- app/Models/Mimic.php
+++ app/Models/Mimic.php
@@ -3,10 +3,11 @@
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Hashtag;
 use App\Models\User;
-use App\Helpers\SendPushNotification;
+use App\Traits\MimicTrait;
 
 class Mimic extends Model
 {
+    use MimicTrait;
 
     const TYPE_VIDEO = 1;
     const TYPE_PIC = 2;
@@ -72,7 +73,7 @@ class Mimic extends Model
         foreach ($usernames[0] as $username) {
 
             //substr exlude "@"
-            $user = Hashtag::where(['username' => substr($username, 1)])->first();
+            $user = User::where(['username' => substr($username, 1)])->first();
 
             if (!empty($user)) {
                 //send notification
@@ -107,117 +108,6 @@ class Mimic extends Model
         return $mimicsResponse;
     }
 
-    /**
-     * generate mimic response
-     * @param  [type] $mimic       [Mimic model]
-     * @param  [type] $hashtags    [array of hashtags in form: [hashtag id] => hashtag name ]
-     * @param  [type] $taggedUsers [array of usernames in form: [user id] => username]
-     * @param  [type] $mimicResponses [all responses of a specific origina mimic, ordered descending by upvotes]
-     * @return [type]              [description]
-     */
-    private function generateMimicResponse($mimic, $hashtags, $taggedUsers, $mimicResponses)
-    {
-        $mimic = 
-        [
-            'id' => $mimic->id,
-            'user' => $mimic->user->username,
-            'user_id' => $mimic->user_id,
-            'mimic_type' => $this->getMimicType($mimic),
-            'upvote' => $mimic->upvote,
-            'file' => $mimic->file,
-        ];
-
-        $hashTagsTmp = [];
-
-        //it could be an array generated with  checkTags
-        if(is_array($hashtags)) {
-            foreach ($hashtags as $hashtag_id => $hashtag_name) {
-                $hashTagsTmp[] = 
-                [
-                    "hashtag_id" => $hashtag_id,
-                    "hashtag_name" => $hashtag_name
-                ];
-            }
-        }
-        //if it's object from database
-        else if(is_object($hashtags)) {
-            foreach ($hashtags as $hashtag) {
-                $hashTagsTmp[] = 
-                [
-                    "hashtag_id" => $hashtag->id,
-                    "hashtag_name" => $hashtag->name,
-                ];
-            }
-        }
-        
-        $taggedUsersTmp = [];
-        //it could be an array generated with  checkTaggedUser
-        if(is_array($taggedUsers)) {
-            foreach ($taggedUsers as $user_id => $username) {
-                $taggedUsersTmp[] = 
-                [
-                    "user_id" => $user_id,
-                    "username" => $username,
-                ];
-            }
-        }
-        //if it's object from database
-        else if(is_object($taggedUsers)) {
-            foreach ($taggedUsers as $taggedUser) {
-                $taggedUsersTmp[] = 
-                [
-                    "user_id" => $taggedUser->id,
-                    "username" => $taggedUser->username
-                ];
-            }
-        }
-
-        $mimicResponsesTmp = [];
-        //get all mimic responses
-        foreach ($mimicResponses as $mimicResponse) {
-            $mimicResponsesTmp[] = $mimicResponse->responseMimic;
-        }
-
-        return 
-        [
-            'mimic' => $mimic,
-            'hashtags' => $hashTagsTmp,
-            'taggedUsers' => $taggedUsersTmp,
-            'mimicResponses' => $mimicResponsesTmp
-        ];
-    }
-
-    /**
-     * get mimic type: video, image
-     * @param  [type] $mimic [Mimic model]
-     */
-    private function getMimicType($mimic)
-    {
-        switch ($mimic->mimic_type) {
-            case Mimic::TYPE_VIDEO:
-                return 'video';
-                break;
-            case Mimic::TYPE_PIC:
-                return 'picture';
-                break;
-        }
-    }
-    /**
-     * send notification to a user if someone tags him/her
-     * @param  $user [User model]
-     */
-    private function sendMimicTagNotification($user)
-    {
-        $data =
-            [
-                'badge' => 1,
-                'sound' => 'default',
-                'title' => trans('core.notifications.respond_to_mimic_title'),
-                'body' => trans('core.notifications.respond_to_mimic_body'),
-            ];
-
-        SendPushNotification::sendNotification($user->id, $data);
-    }
 
     public function user()
     {
