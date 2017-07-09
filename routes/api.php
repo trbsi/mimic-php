4aad71ce0fb64d63210e2459fe98430898e44fb6
Dario Trbovic <timmy.dario@gmail.com>
2017-07-09 15:07:40 +0200

    updated route

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ $api->version('v1', function (Router $api) {
             $api->group(['prefix' => 'mimic'], function (Router $api) {
                 $api->post('add', ['uses' => 'App\Api\V1\Controllers\Mimic\MimicController@addMimic']);
                 $api->get('list', ['uses' => 'App\Api\V1\Controllers\Mimic\MimicController@listMimics']);
-                $api->get('loadResponses', ['uses' => 'App\Api\V1\Controllers\Mimic\MimicController@loadResponses']);
+                $api->get('load-responses', ['uses' => 'App\Api\V1\Controllers\Mimic\MimicController@loadResponses']);
             });
 
         });
