da122d5b838a8a8c47a0d6db2f6d0ee5b123c08e
Dario Trbovic <timmy.dario@gmail.com>
2017-07-23 23:46:18 +0200

    get user own profile

--- app/Api/V1/Controllers/Profile/ProfileController.php
+++ app/Api/V1/Controllers/Profile/ProfileController.php
@@ -16,11 +16,17 @@ class ProfileController extends BaseAuthController
     public function userProfile(Request $request)
     {
         try {
-            $userTable = $this->user->getTable();
+            //if this is set user is accessing other user's profile
+            if($request->id) {
+                $id = $request->id;
+            } else {
+                $id = $this->authUser->id;
+            }
 
+            $userTable = $this->user->getTable();
             return User::select("$userTable.*")
                 ->selectRaw("IF(EXISTS(SELECT null FROM " . (new Follow)->getTable() . " WHERE followed_by = " . $this->authUser->id . " AND following = $userTable.id),1,0) AS i_am_following_you")
-                ->find($request->id);
+                ->find($id);
         } catch (\Exception $e) {
             abort(404, trans('core.user.user_not_found'));
         }
