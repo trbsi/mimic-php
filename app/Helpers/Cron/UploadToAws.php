4ba88dc1149e0158fc1b2ceeb3c8ad16ccfed0bf
Dario Trbovic <timmy.dario@gmail.com>
2017-08-22 19:15:36 +0200

    set upload to aws cron

---
+++ app/Helpers/Cron/UploadToAws.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Helpers\Cron;
+
+use App\Models\Mimic;
+use App\Helpers\FileUpload;
+
+class UploadToAws
+{
+
+    public function __construct(Mimic $mimic, FileUpload $fileUpload)
+    {
+        $this->mimic = $mimic;
+        $this->fileUpload = $fileUpload;
+    }
+
+    public static function upload()
+    {
+        //get 2 mimics where aws_file is null
+        foreach ($this->mimic->whereNull('aws_file')->limit(3)->get() as $model) {
+            //remove "/" from the beginning of a string
+            $path = $this->mimic->getFileOrPath($model->user_id, null, $model);
+            $url = $this->fileUpload->upload(public_path().$path.$model->file, ltrim($path, "/"), null, 'aws');
+
+            $model->update(['aws_file' => $url]);
+        }
+    }
+}
--- app/Http/Controllers/Cron/CronController.php
+++ app/Http/Controllers/Cron/CronController.php
@@ -3,30 +3,8 @@
 namespace App\Http\Controllers\Cron;
 
 use App\Http\Controllers\Controller;
-use App\Models\Mimic;
-use App\Helpers\FileUpload;
-use File;
 
 class CronController extends Controller
 {
-    public function __construct(Mimic $mimic, FileUpload $fileUpload)
-    {
-        $this->mimic = $mimic;
-        $this->fileUpload = $fileUpload;
-    }
 
-    /**
-     * Upload 
-     */
-    public function uploadToAws()
-    {
-        //get 2 mimics where aws_file is null
-        foreach ($this->mimic->whereNull('aws_file')->limit(2)->get() as $model) {
-            //remove "/" from the beginning of a string
-            $path = $this->mimic->getFileOrPath($model->user_id, null, $model);
-            $url = $this->fileUpload->upload(public_path().$path.$model->file, ltrim($path, "/"), null, 'aws');
-
-            $model->update(['aws_file' => $url]);
-        }
-    }
 }    
