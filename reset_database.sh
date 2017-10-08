92e1b60b1b54b9ac9a12d69cce39779cbfa6fc42
Dario Trbovic <timmy.dario@gmail.com>
2017-10-08 15:32:22 +0200

    sh updated

--- reset_database.sh
+++ reset_database.sh
@@ -3,7 +3,6 @@ read -p "DO YOU REALLY WANT TO RESET A DATABASE? " choice
 case "$choice" in 
   * ) echo "BE CAREFUL WHAT YOU WISH FORM";
 esac
-
 read -p "LIKE REALLY REALLY? " choice
 case "$choice" in 
   zelim ) php artisan migrate:reset; php artisan migrate; php artisan db:seed;;
