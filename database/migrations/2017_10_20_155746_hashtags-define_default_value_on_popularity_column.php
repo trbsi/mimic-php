c4f01957bde05c91bbc698e487a2d22303027ec9
Dario Trbovic <timmy.dario@gmail.com>
2017-10-20 18:04:29 +0200

    add migration to define default value on popularity column

---
+++ database/migrations/2017_10_20_155746_hashtags-define_default_value_on_popularity_column.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class HashtagsDefineDefaultValueOnPopularityColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hashtags', function (Blueprint $table) {
+            $table->integer('popularity')->default(1)->change();
+        });
+    }
+}
