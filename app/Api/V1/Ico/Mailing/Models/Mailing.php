5d9157d36d1487a3b167c0b16928ce70acf0529c
Dario Trbovic <timmy.dario@gmail.com>
2017-11-14 20:59:29 +0100

    Set name as fillable

--- app/Api/V1/Ico/Mailing/Models/Mailing.php
+++ app/Api/V1/Ico/Mailing/Models/Mailing.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Mailing extends Model
 {
     protected $table = 'ico_mailings';
-    protected $fillable = ['id', 'first_name', 'last_name', 'email'];
+    protected $fillable = ['id', 'name', 'email'];
     protected $casts =
     [
         'id' => 'int',
